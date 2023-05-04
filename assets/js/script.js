d1378dcfae61dbdbf734e5c5dc4638f03c4b66d8
nam nguyen <n675n055@ku.edu>
2023-05-04 16:17:33 -0500

    put fetch in its own function and added commments

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
   fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyCode}/${conversionCode}.json`)
     .then((response) => response.json())
     .then((data) => {
+      //empties country display so it doesn't appear when changing currencies
       $("#from-countries-display").empty();
       $("#to-countries-display").empty();
       //takes amount and multiplies it by the data from api to get desired numerical output
@@ -27,81 +28,119 @@ document.querySelector("form").addEventListener("submit", (event) => {
       //displays output in the html
       const resultElement = document.getElementById("result");
       resultElement.innerText = `Result: ${amount} ${currencyCode} = ${formatFloat(convertedAmount)} ${conversionCode}`;
-      //gets the country data of the first currency
-      fetch(`https://restcountries.com/v3.1/currency/${currencyCode}?fields=name,capital,currencies,flags,population,latlng`)
-        .then((response) => response.json())
-        .then((data) => {
-          fromCountry = data;
-          $("#fromCountryLabel").text(`Countries that use ${currencyCode}:`);
-          $("#from-countries").empty();
-          for(var i=0; i<data.length; i++) {//if there are more than 1 country associated with the currency, display all in dropdown list
-            $("#from-countries").append($('<option></option>').val(i).html(fromCountry[i].name.common));
-          }
-          fetch(`https://restcountries.com/v3.1/currency/${conversionCode}?fields=name,capital,currencies,flags,population,latlng`)
-          .then((response) => response.json())
-          .then((data) => {
-            toCountry = data;
-            $("#toCountryLabel").text(`Countries that use ${conversionCode}:`);
-            $("#to-countries").empty();
-            for(var i=0; i<data.length; i++) {
-              $("#to-countries").append($('<option></option>').val(i).html(toCountry[i].name.common));
-            }
-            $("#countryDisplayChoices").css("visibility", "visible");//makes form visible
-          })
-        })
+    
+      getCountryData(currencyCode, true);
+      getCountryData(conversionCode, false);
+      $("#countryDisplayChoices").css("visibility", "visible");//makes form visible
     })
     .catch((error) => {
       console.error(error);
     });
 });
 
+function getCountryData(currencyCode, isFrom) {
+  fetch(`https://restcountries.com/v3.1/currency/${currencyCode}?fields=name,capital,currencies,flags,population,latlng,maps,continents`)
+    .then((response) => response.json())
+    .then((data) => {
+
+      if (isFrom) {
+        var countryLabel = "#fromCountryLabel";
+        var countryId = "#from-countries";
+        fromCountry = data;//stores data into variable to use for outside functions
+      } else {
+        var countryLabel = "#toCountryLabel";
+        var countryId = "#to-countries";
+        toCountry = data;
+      }
+
+      $(countryLabel).text(`Countries that use ${currencyCode}:`);
+      $(countryId).empty();//empties current dropdown list so it doesn't stack with new one
+      for(var i=0; i<data.length; i++) {//if there are more than 1 country associated with the currency, display all in dropdown list
+        $(countryId).append($('<option></option>').val(i).html(data[i].name.common));//populates dropdown list with each country
+      }
+    })    
+    .catch((error) => {
+      console.error(error);
+    });
+
+}
+
+//event listener for country selection that calls display country for the left and right selection
 document.querySelector("#countryDisplayChoices").addEventListener("submit", (event) => {
   event.preventDefault();
+  //gets the value from the dropdowns
   var fromCountryIndex = $("#from-countries").val();
   var toCountryIndex = $("#to-countries").val();
-  console.log("hi");
+
+  //calls display country for first and second country data
   displayCountry(fromCountry[fromCountryIndex], true);
-  displayCountry(toCountry[toCountryIndex])
+  displayCountry(toCountry[toCountryIndex], false);
 });
 
+//creates div elements with data of countries gotten from country fetches
 function displayCountry(countryData, isFrom) {
+  //empties the current displays so it doesn't stack with previous displays
   if(isFrom) {
     $("#from-countries-display").empty();
   } else {
     $("#to-countries-display").empty();
   }
 
-  var img = $('<img />', {src: countryData.flags.png, alt: countryData.flags.alt});
+  var img = $('<img />', {src: countryData.flags.png, alt: countryData.flags.alt});//displays image of country flag with alt
   var div1 = $('<div>');
   div1.text(`Country: ${countryData.name.official}`);
   var div2 = $('<div>');
-  div2.text(`Population: ${countryData.population.toLocaleString("en-US")} people`);
+  div2.text(`Population: ${countryData.population.toLocaleString("en-US")} people`);//converts int into string with , after every three digits
   var div3 = $('<div>');
   div3.text(`Capital: ${countryData.capital[0]}`);
   var div4 = $('<div>');
-  div4.text(`Latitude, Longitude: ${formatFloat(countryData.latlng[0])}, ${formatFloat(countryData.latlng[1])}`);
+  div4.text(`Continent: ${countryData.continents[0]}`);
+  var div5 = $('<div>');
+  div5.text(`Latitude, Longitude: ${formatFloat(countryData.latlng[0])}, ${formatFloat(countryData.latlng[1])}`);
+  var a = $("<a />", {href : countryData.maps.googleMaps ,text : "Google Maps"});
 
   if(isFrom) {
-    $("#from-countries-display").append(img, div1, div2, div3, div4);
+    $("#from-countries-display").append(img, div1, div2, div3, div4, div5, a);
   } else {
-    $("#to-countries-display").append(img, div1, div2, div3, div4);
+    $("#to-countries-display").append(img, div1, div2, div3, div4, div5, a);
   }
 }
 
+//rounds float to nearest 2 nonzero decimals ex. 23.33, 123.55, 0.000012
 function formatFloat(number) {
   // Convert the number to a string
-  var str = number.toString();
+  newNumber = 0;
 
-  if(str.charAt(0) === '0' && str !== '0') {
-    var nonZeroIndex = str.indexOf('.')+1;
-    while ((str.charAt(nonZeroIndex) === '0')){
+  var str = number.toString();
+  var isNegative = false;
+  if (str.charAt(0) === '-') {
+    isNegative = true;
+    str = str.slice(1);
+  }
+
+  if(str.charAt(0) === '0' && str !== '0') {//checks if first digit is 0 without the whole value just being 0
+    var nonZeroIndex = str.indexOf('.')+1;//finds the place of decimal
+    while ((str.charAt(nonZeroIndex) === '0')){//finds the place of the first non zero
       nonZeroIndex++;
     }
-    return (parseFloat(str.substring(0,nonZeroIndex+2)));
-  } else if (str.includes('.')) {
-    return (parseFloat(str).toFixed(2));
+    newNumber = parseFloat(str.substring(0,nonZeroIndex+2));
+    if (isNegative) {
+      newNumber = newNumber * -1;
+    }
+    return(newNumber);
+  } else if (str.includes('.')) {//if there's a decimal, return number with 2 decimal places
+    newNumber = parseFloat(str)
+    if (isNegative) {
+      newNumber = newNumber * -1;
+    }
+    return (newNumber.toFixed(2))
   }
-  else {
-    return (parseFloat(str));
+  else {//if there's no decimal, return number as normal
+    newNumber = parseFloat(str);
+    if (isNegative) {
+      newNumber = newNumber * -1;
+    }
+    return (newNumber)
   }
+
 }
